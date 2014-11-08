b547807ff802ceca4be2e84d5fbba83167d67b07
user <nobody>
2014-11-08 10:55:35 -0800

    auto 61 153 1316

--- sql/timeconnectionthing.sql
+++ sql/timeconnectionthing.sql
@@ -1,3 +1,11 @@
+--DROP TYPE connectiontimesboo CASCADE;
+
+CREATE TYPE connectiontimesboo AS (
+        song bigint,
+        counter integer,
+        strength double precision
+);
+
 CREATE OR REPLACE FUNCTION timeconnectionthingy(_timethingy bigint) RETURNS SETOF connectiontimesboo
     LANGUAGE plpgsql
     AS $$
@@ -13,13 +21,14 @@ BEGIN
 
     _counter := 0;
 
-    FOR _song in SELECT id FROM songs order by played asc nulls last,random() LOOP
+    FOR _song in SELECT id FROM songs order by played desc nulls last,random() LOOP
         _strength := _counter * 2 / _numsongs - 1;
 
         _boo.song := _song;
-        _boo._counter = _counter;
+        _boo.counter = _counter;
         _boo.strength := _strength;
         RETURN NEXT _boo;
+        _counter := _counter + 1;
         -- _timethingy is "like stuff better that hasn't been played in a while"
         PERFORM connectionStrength(_timethingy,_song,_strength,false);
      END LOOP;
