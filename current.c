9de2d7437464037790029585627eb8cf6876daa9
user <nobody>
2016-03-08 04:39:31 +0000

    auto (/extra/user/code/songpicker/current.c) 52 1340

--- current.c
+++ current.c
@@ -8,6 +8,26 @@ extern long unsigned int gladeFileSize;
 #include <gtk/gtk.h>
 #include <stdio.h>
 
+// meh!
+const char* rows[] = {
+  "Title",
+  "Artist",
+  "Album",
+  "Duration",
+  "Rating",
+  "Average",
+  "Played",
+  "Id"
+};
+
+#define NUM_ROWS (sizeof(rows)/sizeof(*rows))
+
+#define LENSTR(a) a, (sizeof(a)-1)
+
+#define assert(a) if(!a) {						\
+	error(23,23,"oops! " #a);					\
+  }
+
 int main (int argc, char ** argv)
 {
   PQinit();
@@ -29,12 +49,38 @@ int main (int argc, char ** argv)
 
   gtk_init(&argc,&argv);
 
+  GtkCssProvider * odd_style = gtk_css_provider_get_default ();
+  GError* error = NULL;
+  gtk_css_provider_load_from_data
+	(odd_style,
+	 LENSTR("GtkLabel { background-color: shade(@base_color, 0.9); }\n"),
+	 &error);
+  assert(error==NULL);
+
   GtkBuilder* builder = gtk_builder_new_from_string(gladeFile,gladeFileSize);
   GtkWidget* top = GTK_WIDGET(gtk_builder_get_object(builder,"top"));
-  GtkListStore* props =
-	GTK_LIST_STORE(gtk_builder_get_object(builder,"properties"));
+  GtkBox* props = GTK_BOX(gtk_builder_get_object(builder,"properties"));
+
   GtkLabel* title =
   	GTK_LABEL(gtk_builder_get_object(builder,"title"));
+
+  GtkLabel* labels[NUM_ROWS];
+  int i;
+  for(i=0;i<NUM_ROWS;++i) {
+	GtkLabel* name = gtk_label_new(rows[i]);
+	labels[i] = gtk_label_new(NULL);
+	GtkBox* box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,0);
+	if(i % 2 == 1) {
+	  gtk_style_context_add_provider
+		(GTK_STYLE_PROVIDER(odd_style),
+		 gtk_widget_get_style_context(GTK_WIDGET(box)),
+		 GTK_STYLE_PROVIDER_PRIORITY_USER);
+	}
+
+	gtk_box_pack_start(box,name,false,false,2);
+	gtk_box_pack_start(box,labels[i],true,true,2);
+  }
+
   gboolean update_properties(gpointer udate) {
 	PGresult* result =
 	  logExecPrepared(PQconn,"getTopSong",
@@ -43,7 +89,6 @@ int main (int argc, char ** argv)
 	if(PQntuples(result) == 0) {
 	  puts("(no reply)");
 	} else {
-	  gtk_list_store_clear(props);
 	  for(;i<PQnfields(result);++i) {
 		const char* value = PQgetvalue(result,0,i);
 		if(value == NULL) {
@@ -66,19 +111,15 @@ int main (int argc, char ** argv)
 		} else if(i==0) {
 		  gtk_label_set_text(title,value);
 		}
-      
-		gtk_list_store_set(current_row,
-						   props,
-						   0,
-						   PQfname(result,i),
-						   1,
-						   value);
+		gtk_label_set_text(labels[i],value);
 	  }
 	}
 	PQclear(result);
   }
 
+  g_signal_connect(top,"destroy",gtk_main_quit,NULL);
   g_timeout_add_seconds(2, update_properties,NULL);
+  gtk_widget_show_all(top);
   gtk_main();
 
   return 0;
