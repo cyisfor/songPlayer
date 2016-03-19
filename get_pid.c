b41227cd3f28f21f2611491b7ca6cbabfe99ecce
user <nobody>
2016-03-19 04:55:48 +0000

    auto (/home/code/songpicker/pause.c) 79 2009

--- get_pid.c
+++ get_pid.c
@@ -8,25 +8,35 @@
 #include <stdint.h>
 #include <stdlib.h> // atexit
 
-const char* get_pidfile(void) {
-  return configAt("player.pid");
+const char* get_pidloc(void) {
+  return configAt("pids");
 }
 
 int get_pid(const char* application_name, ssize_t len) {
-  int inp = open(get_pidfile(),O_RDONLY);
+  const char* loc = get_pidloc();
+  mkdir(loc,0700);
+  assert(0==chdir(loc));
+  int inp = open(application_name,O_RDONLY);
   if(inp < 0) return inp;
   int ret = 0;
-  if(1!=fscanf(inp,"%d",&ret))
+  char buf[0x100];
+  int amt = read(inp,buf,0x100);
+  assert(amt>0);
+  if(1!=snscanf(buf,amt,"%d",&ret))
 	return -2;
   return ret;
 }
 
 static void get_pid_done(void) {
-  unlink(get_pidfile());
+  if(0==chdir(get_pidloc()))
+	unlink(application_name);
 }
 
 bool declare_pid(void) {
-  int out = open(get_pidfile(),O_WRONLY|O_CREAT);
+  const char* loc = get_pidloc();
+  mkdir(loc,0700);
+  chdir(loc);
+  int out = open(application_name,O_WRONLY|O_CREAT);
   if(0 != lockf(out, F_TLOCK, 0)) {
 	if(errno == EACCES || errno == EAGAIN) {
 	  close(out);
@@ -35,8 +45,11 @@ bool declare_pid(void) {
 	  
 	perror("Bad lock");
 	exit(23);
-  }
-
+  }  
   atexit(get_pid_done);
+  char buf[0x100];
+  ssize_t amt = snprintf(buf,0x100,"%d",getpid());
+  write(out,buf,amt);
+  fsync(out);
   return true;
 }
---
+++ get_pid.h
@@ -0,0 +1,5 @@
+#include <stdlib.h> // ssize_t
+#include <stdbool.h>
+
+int get_pid(const char* application_name, ssize_t len);
+bool declare_pid(void);
