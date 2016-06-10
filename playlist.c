9434dc09fa2e200c26bd5127ebf0711fa07aee2b
user <nobody>
2016-06-10 18:39:00 +0000

    auto (/home/code/songpicker/playlist.c) 50 1473

--- playlist.c
+++ playlist.c
@@ -45,7 +45,9 @@ void listen_for_more(uv_write_t* req, int status) {
 	if(uv_is_closing((uv_handle_t*)req->handle))
 		return;
 	struct client* client = (struct client*)req->handle->data;
-	assert(TRUE==g_hash_table_insert(clients,client,client));
+	if(FALSE==g_hash_table_insert(clients,client,client)) {
+		fprintf(stderr,"Warning: already had client in clients?\n");
+	}
 }
 
 void write_latest(struct client* client) {
@@ -56,10 +58,7 @@ void write_latest(struct client* client) {
 					 listen_for_more);
 }
 
-// meh, recursive dependencies because late starting!
-void on_connect(uv_stream_t* server_handle, int status);
-
-void broadcast_song(uv_tcp_t* server) {
+void get_latest_song() {
 	PGresult* current_song =
 		logExecPrepared(PQconn,"getTopSongPath",
 										0,NULL,NULL,NULL,0);
@@ -69,28 +68,24 @@ void broadcast_song(uv_tcp_t* server) {
 	}
 
 	latest_song[1].len = PQgetlength(current_song,0,0);
-	latest_song[1].base = g_strndup(PQgetvalue(current_song,0,0),
+	const char* base = strrchr(PQgetvalue(current_song,0,0),'/');
+	if(base == NULL)
+		base = PQgetvalue(current_song,0,0);
+	latest_song[1].base = g_strndup(base,
 																	latest_song[1].len);
 	PQcheckClear(current_song);
+}
+
+void broadcast_song(uv_tcp_t* server) {
+	get_latest_song();
 	// be sure clients is clear, before iterating through it
 	// in case uv_write immediately calls the callback
 	// (it doesn't, but just in case)
 	GHashTable* in_use = clients;
 	clients = g_hash_table_new(g_direct_hash,
 															 g_direct_equal);
-	if(in_use == NULL) {
-		// just starting up
-		// okay, we got our first song, so start listening
-		struct sockaddr_in6 address;
-		int r = uv_ip6_addr(getenv("address"), PORT, &address);
-		CHECK(r,"ip6_addr");
-		r = uv_tcp_bind((uv_tcp_t*)server,
-												(const struct sockaddr*)&address,
-										0);
-		CHECK(r,"tcp_bind");
-		r = uv_listen((uv_stream_t*)server,0x1000, on_connect);
-		CHECK(r,"listen");				
-	} else {
+
+	if(in_use != NULL) {
 		g_hash_table_foreach(in_use,
 												 // sorcery, the key is the first arg
 												 // so don't care about arg 2, and 3
@@ -124,20 +119,20 @@ void on_read(uv_stream_t* handle, ssize_t nread, const uv_buf_t * buf) {
 		return;
 	}
 	struct client* client = (struct client*)handle->data;
-	client->buffer = g_byte_array_append(client->buffer, buf->base, nread);
+	g_byte_array_append(client->buffer, buf->base, nread);
 	guint8* where = strstr(client->buffer->data,"\r\n\r\n");
 	// wow is this cheap
 	if(where == NULL) return;
-	client->buffer = g_byte_array_remove_range(client->buffer,
-																						 0,
-																						 where-client->buffer->data);
+	g_byte_array_remove_range(client->buffer,
+														0,
+														where-client->buffer->data);
 	int r = uv_write(&client->write_req,
 									 handle,
 									 &http_start,
 									 1,
 									 after_write);
 }
-		
+
 
 void alloc(uv_handle_t* handle, size_t suggested, uv_buf_t* buf) {
 	struct client* client = (struct client*)handle->data;
@@ -154,6 +149,7 @@ void on_connect(uv_stream_t* server_handle, int status) {
 	struct client* client = g_new(struct client,1);
 	uv_tcp_init(uv_default_loop(),&client->handle);
 	client->handle.data = client;
+	client->buffer = g_byte_array_new();
 	int r = uv_accept(server_handle,(uv_stream_t*)&client->handle);
 	CHECK(r,"accept");
 	uv_read_start((uv_stream_t*)&client->handle, alloc, on_read);
@@ -181,8 +177,8 @@ int main(int argc, char** argv) {
 	preparation_t query[] = {
     {
       "getTopSongPath",
-			"SELECT recordings.path FROM queue "
-			"INNER JOIN recordings ON recordings.id = queue.recording "
+			"SELECT recordings.path FROM queue\n"
+			"INNER JOIN recordings ON recordings.id = queue.recording\n"
 			"ORDER BY queue.id ASC LIMIT 1"
 		}
 	};
@@ -190,16 +186,31 @@ int main(int argc, char** argv) {
 	PQinit();
 	prepareQueries(query);
 
-	http_start = 
+	http_start =
 		uv_buf_init(LIT("HTTP/1.0 200 OK\r\n"
 										"Content-Type: audio/mpegurl\r\n"
 										"\r\n"));
-														
-	
+
+	clients = g_hash_table_new(g_direct_hash,
+														 g_direct_equal);
+
 	uv_tcp_t server;
 	int r = uv_tcp_init(uv_default_loop(), &server);
 	CHECK(r,"tcp_init");
 	r = uv_tcp_keepalive(&server,1,60);
 	CHECK(r,"tcp_keepalive");
+
+	// okay, we got our first song, so start listening
+	get_latest_song();
+	struct sockaddr_in6 address;
+	r = uv_ip6_addr(host, PORT, &address);
+	CHECK(r,"ip6_addr");
+	r = uv_tcp_bind(&server,
+									(const struct sockaddr*)&address,
+									0);
+	CHECK(r,"tcp_bind");
+	r = uv_listen((uv_stream_t*)&server,0x1000, on_connect);
+	CHECK(r,"listen");
+
 	onNext((void*)broadcast_song,&server);
 }
