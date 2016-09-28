9368baaf5e19f5accff2e0aafc1da1c256a2f07d
user <nobody>
2016-09-28 22:08:23 +0000

    auto (collect-deps.lua) 53 825

--- lx.mk
+++ lx.mk
@@ -1,8 +1,7 @@
+CFLAGS=-fpic `pkg-config --cflags lxpanel gtk+-2.0`
+LDFLAGS=`pkg-config --libs lxpanel gtk+-2.0` -lpq -lm
 lib/lxpause.so: lxpause.so.c o/get_pid.os o/pq.os o/config.os o/preparation.os
-	gcc $(CFLAGS) `pkg-config --cflags lxpanel gtk+-2.0` -o $@ $^ $(LDFLAGS) `pkg-config --libs lxpanel gtk+-2.0` -lpq -lm
-
-lib/lxpause.so: CFLAGS+=-fpic
-lib/lxpause.so: LDFLAGS+=-shared
+	gcc -shared $(CFLAGS) -o $@ $^ $(LDFLAGS) 
 
 o/%.os: %.c
-	$(CC) -fpic $(CFLAGS) -c -o $@ $<
+	$(CC) $(CFLAGS) -c -o $@ $<
--- make/collect-deps.lua
+++ make/collect-deps.lua
@@ -7,11 +7,13 @@ local dependencies = l.Ct(l.P{dependency + 1 * l.V(1)}^0)
 
 local deps = {}
 
+cflags="-pthread -I/usr/include/gtk-2.0 -I/usr/lib/gtk-2.0/include -I/usr/include/pango-1.0 -I/usr/include/atk-1.0 -I/usr/include/cairo -I/usr/include/pixman-1 -I/usr/include/libdrm -I/usr/include/gdk-pixbuf-2.0 -I/usr/include/libpng16 -I/usr/include/pango-1.0 -I/usr/include/freetype2 -I/usr/include/libpng16 -I/usr/include/harfbuzz -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include -I/usr/include/freetype2 -I/usr/include/libpng16 -I/usr/include/harfbuzz -I/usr/include/gio-unix-2.0/ -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include"
+
 local function collect(src)
     if deps[src] then return end
     deps[src] = true
     src = src:sub(1,-2)..'c'
-    local inp = io.popen("gcc -MM "..src)
+    local inp = io.popen("gcc -MM "..cflags.." "..src)
     local line = inp:read('*a')
     if not inp:close() then return end
 
