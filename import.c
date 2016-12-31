5e5b9ea485cdafd35525128025ded9e74344132f
user <nobody>
2016-12-31 04:03:26 +0000

    auto (import.c) 455 3250

--- import.c
+++ import.c
@@ -43,6 +43,9 @@ int forkpipe(int notpipe[2]) {
     return pid;
 }
 
+#define DERPVAL(a) PGgetvalue(a,0,0)
+#define DERPLEN(a) PGgetlength(a,0,0)
+
 preparation emptyHashes,
 	setHash,
 	_checkPath,
@@ -61,85 +64,100 @@ static void fixEmptyHashes(void) {
                                            NULL,
                                            NULL,
                                            NULL,
-                                           0);
+                                           1);
         PQassert(empties,empties && PQresultStatus(empties)==PGRES_TUPLES_OK);
         if(PQntuples(empties)==0) break;
         PQbegin();
         int i = 0;
         for(;i<PQntuples(empties);++i) {
             char* id = PQgetvalue(empties,i,0);
-            char* path = PQgetvalue(empties,i,1);
+						char* path = PQgetvalue(empties,i,1);
             if(path[0]=='\0') continue;
-            printf("Path %s\n",path);
+						path[PQgetlength(empties,i,1)]
 
-            char* h = hash(path);
-            const char* values[2] = { id, h };
-            int lengths[2] = { strlen(id), strlen(h) };
-            int fmt[2] = { 0, 0 };
+            const char* values[2] = { id, hash(path) };
+            int lengths[2] = { PQgetlength(empties,i,0), hash_length };
+            int fmt[2] = { 1, 1 };
             PQcheckClear(prepare_exec(setHash,
                                    2,
                                    values,
                                    lengths,
                                    fmt,
-                                   0));
+                                   1));
             free(h);
         }
         PQcommit();
+				PQcheckClear(empties);
     }
 
 }
 
-char* findWhatB(preparation what, const char* uniq, ssize_t len) {
-    if(uniq==NULL) return NULL;
-    const char* values[1] = { uniq };
-    int lengths[1] = { len };
+PGResult* findWhat(preparation what, string uniq) {
+    if(uniq.base==NULL) return NULL;
+    const char* values[1] = { uniq.base };
+    int lengths[1] = { uniq.len };
     int fmt[1] = { 1 };
     PGresult* r = prepare_exec(what,
 															 1,
-															 values, lengths, fmt, 0);
+															 values, lengths, fmt, 1);
     PQassert(r,r && PQresultStatus(r)==PGRES_TUPLES_OK);
     assert(PQntuples(r)==1);
-    char* id = strdup(PQgetvalue(r,0,0));
-    PQclear(r);
-    return id;
+		return r;
 }
 
-#define findWhat(a,b) findWhatB(a,b,strlen(b))
-
-void setWhat(preparation what, const char* thing, ssize_t len, const char* id) {
+void setWhat(preparation what, string thing, PGresult* id) {
     if(thing==NULL) return;
-    const char* values[2] = { id, thing };
-    int lengths[2] = { strlen(id), len };
-    int fmt[2] = { 0, 1 };
+    const char* values[2] = { DERPVAL(id), thing.base };
+    int lengths[2] = { DERPLEN(id), thing.len };
+    int fmt[2] = { 1, 1 };
     PGresult* r = prepare_exec(what,
 															 2,
-															 values, lengths, fmt, 0);
+															 values, lengths, fmt, 1);
+    PQassert(r,r && PQresultStatus(r)==PGRES_COMMAND_OK);
+    PQclear(r);
+}
+void setWhatCsux(preparation what, PGResult* thing, string id) {
+    if(thing==NULL) return;
+    const char* values[2] = { DERPVAL(id), DERPVAL(thing) };
+    int lengths[2] = { DERPLEN(id), DERPLEN(thing) };
+    int fmt[2] = { 1, 1 };
+    PGresult* r = prepare_exec(what,
+															 2,
+															 values, lengths, fmt, 1);
     PQassert(r,r && PQresultStatus(r)==PGRES_COMMAND_OK);
     PQclear(r);
 }
 
-char* findRecording(const char* song, const char* recorded, const char* artist, const char* path) {
-    char* id = findWhatB(_findRecording,hash(path),hash_length);
+PGresult* findRecording(PGResult* song, string recorded, PGResult* artist, string path) {
+	string csux = {
+		hash(path),
+		hash_length
+	};
+	PGResult* id = findWhat(_findRecording,csux);
 
-    const char* values[4] = { song, recorded, artist, id};
-    int lengths[4] = { strlen(song), recorded ? strlen(recorded) : 0, artist ? strlen(artist) : 0, strlen(id) };
-    int fmt[4] = { 0, 0, 0, 0};
-    PGresult* r = prepare_exec(updateRecording,
-                                 4,
-                                 values,lengths,fmt,0);
-    puts("beep");
-    PQassert(r,r&&PQresultStatus(r)==PGRES_COMMAND_OK);
-    PQclear(r);
-    return id;
+	const char* values[4] = { DERPVAL(song), recorded.base, DERPVAL(artist), DERPVAL(id)};
+	int lengths[4] = { DERPLEN(song),
+										 recorded.len,
+										 DERPLEN(artist),
+										 DERPLEN(id) };
+	int fmt[4] = { 1, 1, 1, 1};
+	PGresult* r = prepare_exec(updateRecording,
+														 4,
+														 values,lengths,fmt,1);
+	PQcheckClear(song);
+	PQcheckClear(artist);
+	PQassert(r,r&&PQresultStatus(r)==PGRES_COMMAND_OK);
+	PQclear(r);
+	return id;
 }
 
 bool checkPath(const char* path) {
     const char* values[1] = { path };
     int lengths[1] = { strlen(path) };
-    int fmt[1] = {0};
+    int fmt[1] = {1};
     PGresult* r = prepare_exec(_checkPath,
                                  1,
-                                 values,lengths,fmt,0);
+                                 values,lengths,fmt,1);
     if(PQresultStatus(r)==PGRES_TUPLES_OK) {
         if(PQntuples(r) == 1) {
             PQclear(r);
@@ -192,16 +210,23 @@ int main(void) {
         realpath(relpath,path);
         printf("real path %s\n",path);
         if(checkPath(path)) continue;
-        char* name = strrchr(path,'/');
-        if(name) {
+				string csux(void) {
+					char* name = strrchr(path,'/');
+					struct string csux = {};
+					if(name) {
             char* dot = strrchr(path,'.');
             long wheredot = (long)dot - (long)path;
             if(wheredot>7) {
                 if(memcmp(dot-7,".vorbis",7)==0)
                     dot = dot - 7;
             }
-            name = strndup(name+1,dot?(long)dot-(long)name-1:strlen(name));
-        }
+
+						csux.len = dot?(long)dot-(long)name-1:strlen(name);
+            csux.base = memdup(name+1,csux.len);
+					}
+					return csux;
+				}
+        string name = csux();
         int io[2];
         int pid = forkpipe(io);
         if(pid==0) {
@@ -221,7 +246,7 @@ int main(void) {
             if(eqs==NULL) continue;
             *eqs = '\0';
             ++eqs; // the space
-            if(0==STRCMP(line,"title",len)) 
+            if(0==STRCMP(line,"title",len))
 							STRINGDUP(title,eqs+1,line+amt-(eqs+1));
             else if(0==STRCMP(line,"artist",len))
 							STRINGDUP(artist,eqs+1,line+amt-(eqs+1));
@@ -230,9 +255,9 @@ int main(void) {
                 strptime(eqs+1,"%Y-%m-%d %H:%M:%S",&date);
                 gotDate = 1;
             } else if(0==STRCMP(line,"album",len))
-							album = STRINGDUP(title,eqs+1,line+amt-(eqs+1));
+							STRINGDUP(album,eqs+1,line+amt-(eqs+1));
 
-            if(artist && title && album && gotDate) break;
+            if(artist.base && title.base && album.base && gotDate) break;
         }
         if(gotDate==0) {
             time_t now = time(NULL);
@@ -241,54 +266,51 @@ int main(void) {
         }
         fclose(info);
         waitpid(pid,NULL,0);
-        if(title && title[0]=='\0') {
-            free(title);
-            title = NULL;
+        if(title.len == 0) {
+            free(title.base);
+            title.base = NULL;
         }
-        puts("beep");
-
-        if(title==NULL) {
-            title = name;
-            printf("yay %s\n",title);
+				if(title.base==NULL) {
+            title.base = name.base;
+						title.len = name.len;
         } else {
-            free(name);
-            name = NULL;
+            free(name.base);
+            name.base = NULL;
         }
-        printf("Whee '%s' '%s' '%s' '%d'\n",title,artist,album,date.tm_year);
+        printf("Whee '%s' '%s' '%s' '%d'\n",title.base,artist.base,album.base,date.tm_year);
 
         PQbegin();
 
-        char* songid = findWhat(findSong,UNSTR(title));
+        PGResult* songid = findWhat(findSong,title);
         free(title.base);
-        char* artistid = findWhat(findArtist,UNSTR(artist));
+        PGResult* artistid = findWhat(findArtist,artist);
         free(artist.base);
-        char* albumid = findWhat(findAlbum,UNSTR(album));
+        PGResult* albumid = findWhat(findAlbum,album);
         free(album.base);
 
         PQcommit();
         PQbegin();
 
-        printf("loul %s %s %s\n",songid,artistid,albumid);
-
-        char* recorded = NULL;
+        string recorded = {};
         if(gotDate) {
             char* template = alloca(0x26); // bleh
             assert(strftime(template,23,"%Y-%m-%d %H:%M:%S",&date)>0);
             strcat(template,".%d");
-            recorded = alloca(0x40);
-            snprintf(recorded,0x40,template,random());
+            recorded.base = alloca(0x40);
+            recorded.len = snprintf(recorded.base,0x40,template,random());
             printf("got date %s\n",recorded);
             // these are the ends you bring me to postgresql!
         }
-        char* recording = findRecording(songid,recorded,artistid,path);
+        PGresult* recording = findRecording(songid,recorded,artistid,path);
         assert(recording);
-        free(songid);
-        free(artistid);
-        printf("Recording %s\n",recording);
+        PQclear(songid);
+        PQclear(artistid);
         setWhat(setPath,path,recording);
-        if(albumid)
-            setWhat(setAlbum,albumid,recording);
-        free(albumid);
+        if(albumid) {
+            setWhatCsux(setAlbum,albumid,recording);
+						PQclear(albumid);
+				}
+				PQclear(recording);
 
         PQcommit();
 
