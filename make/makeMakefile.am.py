5ad5255ee8211426d78ea57a75ca1e0988581e07
autocommit <autocommit>
2018-02-09 21:16:45 +0000

    auto 0 104 1605

---
+++ make/makeMakefile.am.py
@@ -0,0 +1,69 @@
+# sigh
+
+class Package:
+	sources = None
+	def __init__(self, name):
+    self.name = name
+	def added(self,parent):
+		parent.addCflags("$(" + self.name + "_CFLAGS)")
+		parent.addLDflags("$(" + self.name + "_LIBS)")
+
+class pthread:
+	sources = None
+	def added(self,parent):
+		parent.addCflags("-pthread")
+		
+class Program:
+	def __init__(self):
+		self.sources = []
+		self.cflags = []
+		self.ldflags = []
+	def addCflags(self,flags):
+		self.cflags.append(flags)
+	def addLDflags(self,flags):
+		self.ldflags.append(flags)
+	def add(self,thing):
+		if isinstance(thing,str):
+			self.sources.append(thing)
+			return
+		thing.added(self)
+		if thing.sources:
+			self.sources.extend(thing.sources)
+
+class SongDB:
+	def added(self,parent):
+		parent.addLDflags("libsongdb.la")
+		parent.addCflags("$(DB_CFLAGS)")
+songdb = SongDb()
+
+class Pkg:
+	MEDIA = Package("MEDIA")
+	GUI = Package("GUI")
+
+class Glade:
+	def added(self,parent):
+		parent.sources.append(parent.name + ".glade.ch")
+glade = Glade()
+
+class Q(Package):
+	sources = ("queue.c",)
+	def __init__(self):
+    super(Q, self).__init__("GLIB")
+queue = Q()
+
+def program(name,args):
+	p = Program(name)
+	for arg in args:
+		p.add(arg)
+	print(p.name+"_SOURCES =",*p.sources)
+	if p.cflags:
+		print(p.name+"_CFLAGS =",*p.cflags)
+	if p.lbflags:
+		print(p.name+"_LDADD =",*p.ldflags)
+
+program('addalbum',songdb)
+program('best',songdb,Pkg.MEDIA)
+program('current',songdb,Pkg.GUI,glade)
+program('done',songdb,"adjust.c",queue,"select.c","synchronize.c")
+program('dscanner',songdb,Pkg.MEDIA)
+program('enqueue',songdb,queue)
