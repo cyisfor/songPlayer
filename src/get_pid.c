568fc66065f1f8e411ca4a270cd60302806204e9
autocommit <autocommit>
2018-02-09 22:02:21 +0000

    auto 0 337 4712

--- src/get_pid.c
+++ src/get_pid.c
@@ -56,16 +56,15 @@ bool declare_pid(const char* application_name) {
 	// aren't race conditions wonderful?
 	error(0,errno,"no lock? %d\n",info.l_pid);
 	info.l_type = F_WRLCK;
-	if(0 == fcntl(out, F_SETLK, &info)) {
-		return true;
+	if(0 != fcntl(out, F_SETLK, &info)) {
+		if(errno == EACCES || errno == EAGAIN) {
+			close(out);
+			error(0,0,"PID is %d\n",info.l_pid);
+			return false;
+		}
+		perror("Bad lock");
+		abort();
 	}
-	if(errno == EACCES || errno == EAGAIN) {
-		close(out);
-		error(0,0,"PID is %d\n",info.l_pid);
-		return false;
-	}
-	perror("Bad lock");
-	abort();
 	
 	atexit(get_pid_done);
 	char buf[0x100];
