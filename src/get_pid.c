418d4a532d0bb8794989234285c1d0fbe62c82ef
autocommit <autocommit>
2018-02-09 19:19:04 +0000

    auto 0 479 4697

--- src/get_pid.c
+++ src/get_pid.c
@@ -16,46 +16,46 @@
 const char* g_application_name = "song-player";
 
 const char* get_pidloc(void) {
-  return configAt("pids");
+	return configAt("pids");
 }
 
 int get_pid(const char* application_name, ssize_t len) {
-  const char* loc = get_pidloc();
-  mkdir(loc,0700);
-  assert(0==chdir(loc));
-  int inp = open(application_name,O_RDONLY);
-  if(inp < 0) return inp;
-  char buf[0x100];
-  int amt = read(inp,buf,0x100);
-  buf[amt] = '\0';
-  assert(amt>0);
-  return atoi(buf);
+	const char* loc = get_pidloc();
+	mkdir(loc,0700);
+	assert(0==chdir(loc));
+	int inp = open(application_name,O_RDONLY);
+	if(inp < 0) return inp;
+	char buf[0x100];
+	int amt = read(inp,buf,0x100);
+	buf[amt] = '\0';
+	assert(amt>0);
+	return atoi(buf);
 }
 
 static void get_pid_done(void) {
-  if(0==chdir(get_pidloc()))
-	unlink(g_application_name);
+	if(0==chdir(get_pidloc()))
+		unlink(g_application_name);
 }
 
 bool declare_pid(const char* application_name) {
-  g_application_name = application_name;
-  const char* loc = get_pidloc();
-  mkdir(loc,0700);
-  chdir(loc);
-  int out = open(application_name,O_WRONLY|O_CREAT|O_TRUNC,0600);
-  if(0 != lockf(out, F_TLOCK, 0)) {
-	if(errno == EACCES || errno == EAGAIN) {
-	  close(out);
-	  return false;
-	}
+	g_application_name = application_name;
+	const char* loc = get_pidloc();
+	mkdir(loc,0700);
+	chdir(loc);
+	int out = open(application_name,O_WRONLY|O_CREAT|O_TRUNC,0600);
+	if(0 != lockf(out, F_TLOCK, 0)) {
+		if(errno == EACCES || errno == EAGAIN) {
+			close(out);
+			return false;
+		}
 
-	perror("Bad lock");
-	exit(23);
-  }
-  atexit(get_pid_done);
-  char buf[0x100];
-  ssize_t amt = snprintf(buf,0x100,"%d",getpid());
-  write(out,buf,amt);
-  fsync(out);
-  return true;
+		perror("Bad lock");
+		exit(23);
+	}
+	atexit(get_pid_done);
+	char buf[0x100];
+	ssize_t amt = snprintf(buf,0x100,"%d",getpid());
+	write(out,buf,amt);
+	fsync(out);
+	return true;
 }
