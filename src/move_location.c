6532867ec30035be78b545691e9c0dbd5827bc83
autocommit <autocommit>
2019-03-16 00:17:22 +0000

    auto 0 10 205

--- src/move_location.c
+++ src/move_location.c
@@ -24,6 +24,9 @@ int main(int argc, char *argv[])
 	memcpy(like,src,srclen);
 	like[srclen] = '%';
 	like[srclen+1] = 0;
+
+	PQinit();
+	
 	preparation find = prepare(
 			"SELECT id, path FROM recordings "
 			"WHERE encode(path,'escape') LIKE $1::text LIMIT 500");
@@ -40,61 +43,63 @@ int main(int argc, char *argv[])
 	for(;;) {
 		
 		PGresult* result = prepare_exec(find, 1,
-																	values,
-																	lengths,
-																	fmt,
-																	1);
+																		values,
+																		lengths,
+																		fmt,
+																		1);
 		PQassert(result,result && PQresultStatus(result)==PGRES_TUPLES_OK);
 
 		int i;
 		int numrows = PQntuples(result);
 		if(numrows == 0) break;
 		for(i=0;i<numrows;++i) {
-		const char* srcpath = PQgetvalue(result,i,1);
-		assert(srcpath);
-		assert(0==memcmp(srcpath, src, srclen));
-		srcpath += srclen;
-		int restlen = PQgetlength(result,i,1) - srclen;
-		char destpath[destlen + restlen + 1];
-		memcpy(destpath,dest,destlen);
-		memcpy(destpath+destlen,srcpath,restlen);
+			const char* srcpath = PQgetvalue(result,i,1);
+			assert(srcpath);
+			assert(0==memcmp(srcpath, src, srclen));
+			srcpath += srclen;
+			int restlen = PQgetlength(result,i,1) - srclen;
+			char destpath[destlen + restlen + 1];
+			memcpy(destpath,dest,destlen);
+			memcpy(destpath+destlen,srcpath,restlen);
 
-		{
-			const char* v2[] = { PQgetvalue(result,i,0), destpath };
-			const int l2[] = { PQgetlength(result,i,0), destlen+restlen };
-			const int fmt[] = { 1, 1 };
+			{
+				const char* v2[] = { PQgetvalue(result,i,0), destpath };
+				const int l2[] = { PQgetlength(result,i,0), destlen+restlen };
+				const int fmt[] = { 1, 1 };
 
-			PQclear(prepare_exec(begin, 0, NULL, NULL, NULL, 0));
+				PQclear(prepare_exec(begin, 0, NULL, NULL, NULL, 0));
 		
-			PQclear(prepare_exec(update, 2, v2, l2, fmt, 1));
-		}
-
-		if(0 != rename(srcpath, destpath)) {
-			if(errno == EXDEV) {
-				int inp = open(srcpath,O_RDONLY);
-				assert(inp >= 0);
-				int out = open(destpath, O_WRONLY | O_CREAT, 0644);
-				assert(out >= 0);
-				for(;;) {
-					ssize_t amt = sendfile(out, inp, NULL, 0x10000);
-					if(amt == 0) break;
-					if(amt < 0) {
-						perror("huh?");
-						exit(2);
-					}
-				}
-				ensure(0==fsync(out));
-				ensure(0==close(out));
-				ensure(0==close(inp));
-			} else {
-				perror("washt");
-				exit(3);
+				PQclear(prepare_exec(update, 2, v2, l2, fmt, 1));
 			}
-		}
 
-		PQclear(prepare_exec(commit, 0, NULL, NULL, NULL, 0));		
+			if(0 != rename(srcpath, destpath)) {
+				if(errno == EXDEV) {
+					int inp = open(srcpath,O_RDONLY);
+					assert(inp >= 0);
+					int out = open(destpath, O_WRONLY | O_CREAT, 0644);
+					assert(out >= 0);
+					for(;;) {
+						ssize_t amt = sendfile(out, inp, NULL, 0x10000);
+						if(amt == 0) break;
+						if(amt < 0) {
+							perror("huh?");
+							exit(2);
+						}
+					}
+					ensure(0==fsync(out));
+					ensure(0==close(out));
+					ensure(0==close(inp));
+				} else {
+					perror("washt");
+					exit(3);
+				}
+			}
+
+			PQclear(prepare_exec(commit, 0, NULL, NULL, NULL, 0));		
 		
+		}
+		puts("try again?");
 	}
-	
 	return 0;
 }
+	
