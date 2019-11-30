f13f934e8a723d134145ec9613215df3203c88ea
user <email>
2019-11-30 19:25:34 +0000

    getting rid of autotools

--- lx.mk
+++
@@ -1,7 +0,0 @@
-CFLAGS=-g -fpic `pkg-config --cflags lxpanel gtk+-2.0`
-LDFLAGS=`pkg-config --libs lxpanel gtk+-2.0` -lpq -lm
-lib/lxpause.so: lxpause.so.c o/get_pid.os o/pq.os o/config.os o/preparation.os o/pause-common.os
-	gcc -shared $(CFLAGS) -o $@ $^ $(LDFLAGS) 
-
-o/%.os: %.c
-	$(CC) $(CFLAGS) -c -o $@ $<
