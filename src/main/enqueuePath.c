0f342e43f53b4c2a5fbd4439c1ba7c30ed0aa23e
autocommit <autocommit>
2020-01-03 08:22:15 +0000

    auto 0 54 594

--- src/main/enqueuePath.c
+++ src/main/enqueuePath.c
@@ -1,5 +1,5 @@
-#include "queue.h"
-#include "pq.h"
+#include "../queue.h"
+#include "../pq.h"
 
 #include <limits.h>
 #include <stdlib.h>
@@ -7,7 +7,7 @@
 int main(int argc, char**argv) {
     int i;
     PQinit();
-		queuePrepare();
+		queue_init();
     for(i=1;i<argc;++i) {
 			char p[PATH_MAX];
         enqueuePath(realpath(argv[i],p));
