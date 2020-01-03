eec615338d52e54370732a9f5f061acea28ee0cc
autocommit <autocommit>
2020-01-03 08:21:05 +0000

    auto 0 49 482

--- src/main/enqueue.c
+++ src/main/enqueue.c
@@ -1,5 +1,5 @@
-#include "queue.h"
-#include "pq.h"
+#include "../queue.h"
+#include "../pq.h"
 
 #include <stdio.h>
 #include <string.h>
@@ -18,7 +18,7 @@ static void byline(void) {
 int main(int argc, char**argv) {
     int i;
     PQinit();
-		queuePrepare();
+		queue_init();
  		if(argc == 1) {
 			byline();
 		} else for(i=1;i<argc;++i) {
